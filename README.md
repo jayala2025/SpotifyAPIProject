feace3e1301c3951d68d861e39c92736ca9d0a1f
Jose Ayala <jayala1@trinity.edu>
2023-06-23 21:04:18 +0000

    Pushing database, updated code, and README file

---
+++ README.md
@@ -0,0 +1,16 @@
+# This project is still in development stages.
+* The goals of this project are:
+	* To make me more comfortable with using APIs, Databases, and Version Control
+	* Have a project that I can use to show my skills
+	* Create a product I actually am passionate about
+# Important notes regarding my project:
+* I use Python 3, SQL, and multiple Python libraries such as:
+	* SQL Alchemy
+	* Pandas
+* I utilize the Spotify Web API to get track information
+# Code functionality:
+* Create an app on Spotify for Developers and acquire Client ID + Secret
+* Specify the header and body information necessary for a POST request
+* Look through the Spotify API Documentation to understand how track information is stored
+* Grab the track information you want using the specified JSON
+* Test by putting in the track ID of your desired track
--- projectStart.py
+++ projectStart.py
@@ -2,8 +2,11 @@ import requests
 import os
 import spotipy
 import json
+import pandas as pd
+import sqlalchemy as db
 from spotipy.oauth2 import SpotifyClientCredentials 
 
+# testTrackID: 48NXpYRuvv9izul4oXhqS9
 # client_id = '7d90581a1f7e4320b41671ac9deab569'
 # client_secret = 'a0e800ffe62b4b79b51e2b8da0ae6bfc'
 url = 'https://accounts.spotify.com/api/token'
@@ -22,6 +25,14 @@ headers = {'Authorization': 'Bearer {token}'.format(token=access_token)}
 baseURL = 'https://api.spotify.com/v1/'
 track_id = input('Enter track id: ')
 trackInfo = requests.get(baseURL + 'tracks/' + track_id, headers=headers)
+
+# trackDataFrame = pd.DataFrame.from_dict(trackInfo.json())
+# engine = db.create_engine('sqlite:///spotifyapiproject.db')
+# trackDataFrame.to_sql('Track Information', con=engine, if_exists='replace', index=False)
+
+# with engine.connect() as connection:
+#    query_result = connection.execute(db.text("SELECT * FROM Track Information;")).fetchall()
+#    print(pd.DataFrame(query_result))
 
 trackName = trackInfo.json()['name']
 trackArtists = trackInfo.json()['artists'][0]['name']
