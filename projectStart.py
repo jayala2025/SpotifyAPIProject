5596722f130da11a6eecca061ebddcd22b90ff69
Jose Ayala <jayala1@trinity.edu>
2023-06-23 17:12:22 +0000

    Uploading post and get requests

---
+++ projectStart.py
@@ -0,0 +1,25 @@
+import requests
+import os
+import spotipy  
+from spotipy.oauth2 import SpotifyClientCredentials 
+
+# client_id = '7d90581a1f7e4320b41671ac9deab569'
+# client_secret = 'a0e800ffe62b4b79b51e2b8da0ae6bfc'
+url = 'https://accounts.spotify.com/api/token'
+auth_response = requests.post(url, {
+'grant_type': 'client_credentials',
+'client_id': '7d90581a1f7e4320b41671ac9deab569',
+'client_secret': 'a0e800ffe62b4b79b51e2b8da0ae6bfc',
+})
+
+auth_response_data = auth_response.json()
+# print(auth_response_data)
+
+access_token = auth_response_data['access_token']
+
+headers = {'Authorization': 'Bearer {token}'.format(token=access_token)}
+baseURL = 'https://api.spotify.com/v1/'
+track_id = input('Enter track id: ')
+trackInfo = requests.get(baseURL + 'tracks/' + track_id, headers=headers)
+
+print(trackInfo.json())
